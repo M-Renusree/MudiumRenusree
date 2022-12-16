2cdd24b1b35c731e12fe276a1deaf0a5d6cb9c87
M-Renusree <120365508+M-Renusree@users.noreply.github.com>
2022-12-16 14:10:03 +0530

    Update twenty four.py

--- twenty four.py
+++ twenty four.py
@@ -1,6 +1,6 @@
 n=input("enter the letter")
 vowels=['a',',e','i','o','u']
 if n in vowels:
-    print("n is vowel")
+    print(n+ " is vowel")
 else:
-    print("n is not vowel")    
+    print(n+" is not vowel")    
